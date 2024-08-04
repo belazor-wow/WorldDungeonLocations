ed1091f0bd2ffc00c960f87b36ea11cb10dd7fba
Fillip H <9424286+filliph@users.noreply.github.com>
2024-08-04 13:07:37 +0300

    chore: Refactor original DataProvider

--- dataProviders/WorldDungeonEntranceDataProvider.lua
+++ dataProviders/WorldDungeonEntranceDataProvider.lua
@@ -56,4 +56,46 @@ function WorldDungeonEntranceDataProviderMixin:RefreshAllData()
     end
 end
 
+-- Create new dungeon entrance pin mixin
+WorldDungeonEntrancePinMixin = CreateFromMixins(DungeonEntrancePinMixin)
+
+--[[
+function WorldDungeonEntrancePinMixin:UpdateMousePropagation()
+    if not InCombatLockdown() then
+        self:SetPropagateMouseClicks(not self:DoesMapTypeAllowSuperTrack());
+    end
+end
+]]
+
+function WorldDungeonEntrancePinMixin:UpdateMousePropagation() end
+function WorldDungeonEntrancePinMixin:DoesMapTypeAllowSuperTrack() return true; end
+
+function WorldDungeonEntrancePinMixin:CheckShowTooltip()
+	if self:UseTooltip() then
+        local instanceId = select(10, EJ_GetInstanceInfo(self.journalInstanceID))
+
+		local tooltip = GetAppropriateTooltip();
+		tooltip:SetOwner(self, "ANCHOR_RIGHT");
+		local name, description = self:GetBestNameAndDescription();
+		GameTooltip_SetTitle(tooltip, name);
+
+		if description then
+			GameTooltip_AddNormalLine(tooltip, description);
+		end
+
+        if private.savedInstances[instanceId] ~= nil then
+            for key, value in pairs(private.savedInstances[instanceId]) do
+                tooltip:AddDoubleLine("|cffffffee" .. key .. "|r", value)
+            end
+        end
+
+		local instructionLine = self:GetTooltipInstructions();
+		if instructionLine then
+			GameTooltip_AddInstructionLine(tooltip, instructionLine, false);
+		end
+
+		tooltip:Show();
+	end
+end
+
 WorldMapFrame:AddDataProvider(WorldDungeonEntranceDataProviderMixin)
